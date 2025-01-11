5b391cedd815ed68b246c97b38ddf6efac42a29f
Ethan Hall <ethanhall7742@gmail.com>
2025-01-11 15:39:10 +0000

    Updated handler function to log error and exit safely

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -1,8 +1,8 @@
 package commands
 
 import (
-	"errors"
 	"fmt"
+	"log"
 	"somewherecosmic/aggregator/internal/config"
 )
 
@@ -12,35 +12,35 @@ type State struct {
 
 type Command struct {
 	Name string
-	args []string
+	Args []string
 }
 
 type Commands struct {
-	validCommands map[string]func(*State, Command) error
+	ValidCommands map[string]func(*State, Command) error
 }
 
-func handerLogin(s *State, cmd Command) error {
-	if len(cmd.args) == 0 {
-		return errors.New("function expects a single argument: username")
+func HandlerLogin(s *State, cmd Command) error {
+	if len(cmd.Args) == 0 {
+		log.Fatal("function expects a single argument: username")
 	}
 
-	if err := s.Conf.SetUser(cmd.args[0]); err != nil {
+	if err := s.Conf.SetUser(cmd.Args[0]); err != nil {
 		return err
 	}
 
-	fmt.Printf("User has been set: %s\n", cmd.args[0])
+	fmt.Printf("User has been set: %s\n", cmd.Args[0])
 
 	return nil
 }
 
-func (c *Commands) register(name string, f func(*State, Command) error) error {
-	c.validCommands[name] = f
+func (c *Commands) Register(name string, f func(*State, Command) error) error {
+	c.ValidCommands[name] = f
 
 	return nil
 }
 
-func (c *Commands) run(s *State, cmd Command) error {
-	err := c.validCommands[cmd.Name](s, cmd)
+func (c *Commands) Run(s *State, cmd Command) error {
+	err := c.ValidCommands[cmd.Name](s, cmd)
 	if err != nil {
 		return err
 	}
--- main.go
+++ main.go
@@ -3,21 +3,37 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"somewherecosmic/aggregator/internal/commands"
 	"somewherecosmic/aggregator/internal/config"
 )
 
 func main() {
-
 	conf, err := config.Read()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	conf.SetUser("etho")
-	newConf, err := config.Read()
-	if err != nil {
-		log.Fatal(err)
+	var state commands.State
+	state.Conf = conf
+
+	commandRegistry := commands.Commands{
+		ValidCommands: make(map[string]func(*commands.State, commands.Command) error),
 	}
 
-	fmt.Println(newConf)
+	commandRegistry.Register("login", commands.HandlerLogin)
+
+	userArgs := os.Args
+	if len(userArgs) < 2 {
+		log.Fatal("Error: Too few arguments provided")
+	}
+
+	issuedCommand := commands.Command{
+		Name: userArgs[1],
+		Args: userArgs[2:],
+	}
+
+	if err := commandRegistry.Run(&state, issuedCommand); err != nil {
+		fmt.Println(err)
+	}
 }
